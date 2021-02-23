e9671e37d0cea50cdd48880564eea5f042d48812
adsonatural <adsonhenriquesilva@gmail.com>
2021-02-23 17:56:14 +0100

    create entity for user

---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 } from 'uuid';
+
+@Entity('users')
+class User {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = v4();
+        }
+    }
+}
+
+export { User };
