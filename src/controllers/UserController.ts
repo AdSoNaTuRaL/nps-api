b4c6528c21478b4b98b335a234218819234fa022
adsonatural <adsonhenriquesilva@gmail.com>
2021-02-24 15:48:39 +0100

    Creating a user repository and refactoring user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../entities/User';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const userRepository = getRepository(User);
+        const userRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await userRepository.findOne({
             email
@@ -25,7 +25,7 @@ class UserController {
 
         await userRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
