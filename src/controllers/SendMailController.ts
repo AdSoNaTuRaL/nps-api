b39789b50b98d908ec5db4a42757efe396a24103
adsonatural <adsonhenriquesilva@gmail.com>
2021-02-25 13:54:59 +0100

    Sending email inside controller

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveyRepository } from "../repositories/SurveyRepository";
 import { SurveyUserRepository } from "../repositories/SurveyUserRepository";
 import { UserRepository } from "../repositories/UserRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -20,9 +21,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveyRepository.findOne({ id: survey_id });
+        const survey = await surveyRepository.findOne({ id: survey_id });
 
-        if (!surveyAlreadyExists) {
+        if (!survey) {
             return response.status(400).json({
                 error: 'Survey does not exists',
             });
@@ -34,6 +35,8 @@ class SendMailController {
         });
 
         await surveyUserRepository.save(surveyUser);
+
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return response.json(surveyUser);
     }
